248f857b8ccb6a2ceb8066d9a52758cdd2a85a2c
YanivGros <qywoec@gmail.com>
2023-08-12 13:26:11 +0300

    fix not square matrix gui

--- OttMap/OttGuiMap.py
+++ OttMap/OttGuiMap.py
@@ -15,7 +15,6 @@ class OttGuiMap(tk.Tk, OttBaseMap):
 
     @wraps(OttBaseMap.__init__)
     def __init__(self, padding=10, color="white", **kwargs):
-
         tk.Tk.__init__(self)
         OttBaseMap.__init__(self, **kwargs)
 
@@ -28,12 +27,18 @@ class OttGuiMap(tk.Tk, OttBaseMap):
         screen_height = self.winfo_screenheight() - 2 * padding
 
         canvas_edge_length = min(screen_width, screen_height)
-        self.square_size = min((canvas_edge_length - 2 * padding) // self.width,
-                               (canvas_edge_length - 2 * padding) // self.length)
+        self.square_size = min(
+            (canvas_edge_length - 2 * padding) // self.width, (canvas_edge_length - 2 * padding) // self.length
+        )
 
         self.geometry(f"{canvas_edge_length}x{canvas_edge_length}")
 
-        self.canvas = tk.Canvas(self, width=canvas_edge_length, height=canvas_edge_length, bg="white")
+        self.canvas = tk.Canvas(
+            self,
+            width=self.square_size * self.width + padding * 2,
+            height=self.square_size * self.length + padding * 2,
+            bg="white",
+        )
         self.canvas.pack(pady=10)
 
         self.tracked_objects: list[OttBaseObject] = []
@@ -48,24 +53,39 @@ class OttGuiMap(tk.Tk, OttBaseMap):
             for j in range(self.length):
                 x1, y1 = i * self.square_size, j * self.square_size
                 x2, y2 = x1 + self.square_size, y1 + self.square_size
-                self.canvas.create_rectangle(x1 + self.padding, y1 + self.padding, x2 + self.padding, y2 + self.padding,
-                                             fill="white",
-                                             outline=OUT_LINE_COLOR,
-                                             width=WIDTH)
+                self.canvas.create_rectangle(
+                    x1 + self.padding,
+                    y1 + self.padding,
+                    x2 + self.padding,
+                    y2 + self.padding,
+                    fill="white",
+                    outline=OUT_LINE_COLOR,
+                    width=WIDTH,
+                )
 
         for obj in self.ott_objects:
             x1, y1 = obj.position[0] * self.square_size, obj.position[1] * self.square_size
             x2, y2 = x1 + self.square_size, y1 + self.square_size
-            self.canvas.create_rectangle(x1 + self.padding, y1 + self.padding, x2 + self.padding, y2 + self.padding,
-                                         fill=obj.get_color(),
-                                         outline=OUT_LINE_COLOR,
-                                         width=WIDTH)
+            self.canvas.create_rectangle(
+                x1 + self.padding,
+                y1 + self.padding,
+                x2 + self.padding,
+                y2 + self.padding,
+                fill=obj.get_color(),
+                outline=OUT_LINE_COLOR,
+                width=WIDTH,
+            )
 
         x1, y1 = self.agent_position[0] * self.square_size, self.agent_position[1] * self.square_size
         x2, y2 = x1 + self.square_size, y1 + self.square_size
-        self.canvas.create_rectangle(x1 + self.padding, y1 + self.padding, x2 + self.padding, y2 + self.padding,
-                                     outline=OUT_LINE_COLOR,
-                                     width=WIDTH * 4)
+        self.canvas.create_rectangle(
+            x1 + self.padding,
+            y1 + self.padding,
+            x2 + self.padding,
+            y2 + self.padding,
+            outline=OUT_LINE_COLOR,
+            width=WIDTH * 4,
+        )
 
     @wraps(OttBaseMap.add_object)
     def update_map(self, **kwargs):
